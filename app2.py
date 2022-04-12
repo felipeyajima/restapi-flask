1f043861ef473da6efca797fea8e3a6b94887c37
Felipe <felipe.yajimabatista@gmail.com>
2022-04-11 21:32:01 -0300

    metrificado

--- app2.py
+++ app2.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from resources.jornada import Jornadas, Jornada
 from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
 app = Flask(__name__)
 api = Api(app)
 
 metrics = RESTfulPrometheusMetrics(app, api)
-metrics.info('first_app', 'This is my first app', version='0.0.1')
-api.add_resource(Jornadas, '/jornadas')
-api.add_resource(Jornada, '/jornada/<string:jornada_id>')
 
+@app.route('/')
+def index():
+    return 'Olá mundo!'
 
 if __name__ == '__main__':
     app.run()
